49be4bf5e3b2e6fe48733bfb26df32e334bfb2e4
Edmosha <dasha1234495@gmail.com>
2021-03-05 14:13:21 +0300

    .

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function numbers(a) {
     let num = 1;
     do {
-        if ((num==5) || (num==13) || (num==22) || (num==35) || (num==98)) {
+        if ((num===5) || (num===13) || (num===22) || (num===35) || (num===98)) {
             let strNum
             strNum = String(num)
             console.log(strNum + " это " + typeof strNum)
